e814e71a595f0b4913f29ace3ea49d1bb449e8d7
sushixnyan <sushixnyan@gmail.com>
2014-03-04 21:06:12 -0500

    colors -> base16, updated fonts

--- kawaii-bar.h
+++ kawaii-bar.h
@@ -5,27 +5,27 @@
 
 #define SYSTRAY_W	20		/* width of system tray in pixels, 0 if none */
 #define PROGRESS_BAR_W	50		/* width of progress bar in pixels */
-static gboolean topbar = TRUE;		/* TRUE for bar to be displayed at top of screen */
+static gboolean topbar	= TRUE;		/* TRUE for bar to be displayed at top of screen */
 
 /* colors */
-static const char *bgcolor	= "#000000";
+static const char *bgcolor	    = "#1d1f21";
 
-static const char *progress_fg	= "#0088cc";
-static const char *progress_bg	= "#4b4b4b";
-static const char *progress_urg	= "#ff0000";
+static const char *progress_fg	    = "#a1efe4";
+static const char *progress_bg	    = "#75715e";
+static const char *progress_urg	    = "#fd971f";
 
-static const char *desktop_norm	= "#646464";
-static const char *desktop_sel	= "#ffffff";
-static const char *desktop_line = "#00c5cd";
+static const char *desktop_norm	    = "#75715e";
+static const char *desktop_sel	    = "#f9f8f5";
+static const char *desktop_line	    = "#fd971f";
 
-static const char *stats_text	= "#00c5cd";
+static const char *stats_text	    = "#fd971f";
 
-static const char *clock_primary = "#1793d1";
-static const char *clock_secondary = "#ffffff";
+static const char *clock_primary    = "#66d9ef";
+static const char *clock_secondary  = "#f9f8f5";
 
 /* fonts */
-static const char *font	= "Sans 12";
-static const char *stats_font = "Sans 9";
+static char *font	    = "Inconsolata 14";
+static char *stats_font	    = "Inconsolata 11";
 
-static const char *clock_font_lg = "Agency FB Bold 18";
-static const char *clock_font_sm = "DIN condensed 8";
+static char *clock_font_lg  = "Agency FB Bold 18";
+static char *clock_font_sm  = "Inconsolata 11";
